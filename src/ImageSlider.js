7e2c4cf2ac5a97e881bc6f18f0ecfbeeac916677
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-06-07 07:27:15 +0000

    Automatically backed up by Learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -2,16 +2,27 @@
 
 import React from 'react';
 
+<<<<<<< HEAD
 class ImageSlider extends React.Component {
   constructor() {
     super();
+=======
+function ImageSlider() {
+  // constructor() {
+  //   super();
+    render() {
+>>>>>>> 172a015cd479fc458a78b08ef0add237b01ec54c
 
     this.state = {
       currentSlideIndex: 0
     };
   }
 
+<<<<<<< HEAD
   render() {
+=======
+
+>>>>>>> 172a015cd479fc458a78b08ef0add237b01ec54c
     return (
       <div>I am on slide 0</div>
     )
